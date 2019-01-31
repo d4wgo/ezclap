71655727f2128a0fb96153b7002d184693dcf739
Daniel Reynolds <danielreynolds101@gmail.com>
2019-01-31 15:41:40 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var http = require('http');
 var path = require('path');
 var app = require('express')();
 var server = require('http').Server(app); 
-app.set('port', 8080);
+app.set('port', 8085);
 app.use('/static', express.static(__dirname + '/static'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/main.html'));
 });
-server.listen(8080, function() {
-    console.log('Starting server on port 8080');
+server.listen(8085, function() {
+    console.log('Starting server on port 8085');
 });
